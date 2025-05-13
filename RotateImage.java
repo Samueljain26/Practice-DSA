33fdc95b4fefdbc8216f45f8e41a8f21ec07676f
Samueljain26 <103981070+Samueljain26@users.noreply.github.com>
2025-05-13 18:00:42 +0530

    [Samuel] ADD: transpose and reverse a matrix -2d array

---
+++ RotateImage.java
@@ -0,0 +1,35 @@
+class RotateImage {
+    public void rotate(int[][] matrix) {
+        // int n = matrix.length;
+        // int[][] temp = new int[n][n];
+
+        // for (int i =0; i<n; i++) {
+        //     for (int j=0; j<n; j++) {
+        //         temp[j][n-i-1] = matrix[i][j];
+        //     }
+        // }
+
+        // for (int i = 0; i <n; i++) {
+        //     for (int j=0; j < n; j++) {
+        //         matrix[i][j] = temp[i][j];
+        //     }
+        // }
+    
+        int n = matrix.length;
+
+        for (int i=0; i<n; i++) {
+            for (int j=i; j<n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i=0; i<n; i++) {
+            for (int j = 0; j < n/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
