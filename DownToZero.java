faea95c2657460317c67356d92aa227e4ef4b762
Samueljain26 <103981070+Samueljain26@users.noreply.github.com>
2025-05-13 17:58:21 +0530

    [Samuel] ADD: down to zero -Dynamic programming

---
+++ DownToZero.java
@@ -0,0 +1,70 @@
+  import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'downToZero' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int downToZero(int n) {
+    int[] dp = new int[n + 1];
+
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            dp[i] = dp[i - 1] + 1;
+
+
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    dp[i] = Math.min(dp[i], dp[i / j] + 1);
+                }
+            }
+        }
+
+        return dp[n];
+
+    }
+
+}
+
+public class DownToZero {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.downToZero(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+
